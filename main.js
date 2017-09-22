ccec871cc0bd4f22a94b644ab4b142c8d9abbf43
cnsheafe <cnsheafe@gmail.com>
2017-09-22 13:54:57 -0500

    added eventlisteners to hide sections

---
+++ main.js
@@ -0,0 +1,26 @@
+let projectLink = document.getElementById("link-projects");
+let contactLink = document.getElementById("link-contact");
+let homePage = document.getElementById("home");
+let projectsPage = document.getElementById("projects");
+let contactPage = document.getElementById("contact");
+
+projectLink.addEventListener("click", event => {
+    event.preventDefault();
+    projectsPage.classList.remove("hide");
+    homePage.classList.add("hide");
+    contactPage.classList.add("hide");
+});
+
+contactLink.addEventListener("click", event => {
+    event.preventDefault();
+    contactPage.classList.remove("hide");
+    projectsPage.classList.add("hide");
+    homePage.classList.add("hide");
+});
+
+homePage.addEventListener("click", event => {
+    event.preventDefault();
+    homePage.classList.remove("hide");
+    contactPage.classList.add("hide");
+    projectsPage.classList.add("hide");
+});
