7a8cf6fe30cd73f7ea808b13437b828a75e8a3b1
cnsheafe <cnsheafe@gmail.com>
2017-09-29 17:49:40 -0600

    moved projects to home page

--- docs/main.js
+++ docs/main.js
@@ -1,27 +1,22 @@
 let homeLink = document.getElementById("link-home");
-let projectLink = document.getElementById("link-projects");
 let contactLink = document.getElementById("link-contact");
 let homePage = document.getElementById("home");
-let projectsPage = document.getElementById("projects");
+let projectSection = document.getElementById("projects");
 let contactPage = document.getElementById("contact");
 
-projectLink.addEventListener("click", event => {
-    event.preventDefault();
-    projectsPage.classList.remove("hide");
-    homePage.classList.add("hide");
-    contactPage.classList.add("hide");
-});
 
 contactLink.addEventListener("click", event => {
     event.preventDefault();
     contactPage.classList.remove("hide");
-    projectsPage.classList.add("hide");
+
+    projectSection.classList.add("hide");
     homePage.classList.add("hide");
 });
 
 homeLink.addEventListener("click", event => {
     event.preventDefault();
     homePage.classList.remove("hide");
+    projectSection.classList.remove("hide");
+
     contactPage.classList.add("hide");
-    projectsPage.classList.add("hide");
 });
