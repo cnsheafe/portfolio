828ec8d6f8a4ba457bb042b62c32d4978b57f169
Chris <cnsheafe@pm.me>
2018-12-23 13:37:36 -0700

    Overhaul html, css, and js

--- docs/main.js
+++ docs/main.js
@@ -1,19 +1,11 @@
-const aboutLink = document.getElementById("link-about");
-const projectsLink = document.getElementById("link-projects");
-const contactLink = document.getElementById("link-contact");
-
-const nav = document.getElementsByTagName("NAV")[0];
-const navBar = document.getElementById("links");
-
-window.addEventListener("scroll", event => {
-    if(window.scrollY >= window.innerHeight) {
-        nav.classList.add("nav-fixed");
-        navBar.classList.remove("navbar");
-        navBar.classList.add("navbar-fixed");
-
-        navBar.querySelectorAll("i").forEach(icons => {
-            icons.classList.add("hide");
-        });
-        navBar.querySelector("img").classList.add("hide");
-    }
-})
+const state = {
+    onLandingPage: false,
+    charter: {
+        showImageModal: false,
+        imgIndex: 0,
+    },
+    fieldboom: {
+        showImageModal: false,
+        imgIndex: 0,
+    },
+};
