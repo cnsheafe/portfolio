0bc9d15208f5648cf134acca2a51163ba32738d3
Chris <cnsheafe@gmail.com>
2018-12-25 22:24:55 -0700

    assign variable for elements charter and fieldboom
    also indent with two spaces instead of four

--- docs/main.js
+++ docs/main.js
@@ -1,86 +1,82 @@
 /*
-Peformance notes: Consider caching document.getElementById('charter') if diff is considerable
 Ergonomics: Consider just adding eslint
 */
 
+const charterElm = document.getElementById('charter')
+const fieldboomElm = document.getElementById('fieldboom')
+
 const state = {
-    onLandingPage: false,
-    charter: {
-        image: {
-            index: 0,
-            showModal: false,
-            subtitles: [
-                'Channel guide for available programs.',
-                'List of related assets for actor "Brad Pitt".',
-                'A user\'s "My Library" showing recently watched titles and saved VODs.'
-            ],
-        },
-        carousel: document.getElementById('charter')
-            .querySelector('.carousel'),
-        carouselLeft: document.getElementById('charter')
-            .querySelector('.carousel__left-arrow'),
-        carouselRight: document.getElementById('charter')
-            .querySelector('.carousel__right-arrow'),
+  onLandingPage: false,
+  charter: {
+    image: {
+      index: 0,
+      showModal: false,
+      subtitles: [
+        'Channel guide for available programs.',
+        'List of related assets for actor "Brad Pitt".',
+        'A user\'s "My Library" showing recently watched titles and saved VODs.'
+      ],
     },
-    fieldboom: {
-        image: {
-            index: 0,
-            showModal: false,
-            subtitles: [],
-        },
-        carousel: document.getElementById('fieldboom')
-            .querySelector('.carousel'),
-        carouselLeft: document.getElementById('fieldboom')
-            .querySelector('.carousel__left-arrow'),
-        carouselRight: document.getElementById('fieldboom')
-            .querySelector('.carousel__right-arrow'),
+    carousel: charterElm.querySelector('.carousel'),
+    carouselLeft: charterElm.querySelector('.carousel__left-arrow'),
+    carouselRight: charterElm.querySelector('.carousel__right-arrow'),
+  },
+  fieldboom: {
+    image: {
+      index: 0,
+      showModal: false,
+      subtitles: [],
     },
+    carousel: fieldboomElm.querySelector('.carousel'),
+    carouselLeft: fieldboomElm.querySelector('.carousel__left-arrow'),
+    carouselRight: fieldboomElm.querySelector('.carousel__right-arrow'),
+  },
 };
 
 
 state.charter.carouselLeft.addEventListener('click', (event) => {
-    const { image } = state.charter
-    if (!image.index) {
-        image.index = 2
-    } else {
-        image.index -= 1
-    }
+  const { image } = state.charter
+  if (!image.index) {
+    image.index = 2
+  } else {
+    image.index -= 1
+  }
 
-    changeActiveCarouselImage(image.index, state.charter)
+  changeActiveCarouselImage(image.index, state.charter)
 })
 
 state.charter.carouselRight.addEventListener('click', (event) => {
-    const { image } = state.charter
-    if (image.index === 2) {
-        image.index = 0
-    } else {
-        image.index += 1
-    }
+  const { image } = state.charter
+  if (image.index === 2) {
+    image.index = 0
+  } else {
+    image.index += 1
+  }
 
-    changeActiveCarouselImage(image.index, state.charter)
+  changeActiveCarouselImage(image.index, state.charter)
 })
 
 function changeActiveCarouselImage(index, job) {
-    const { carousel } = job
-    const liArray = carousel.querySelectorAll('.carousel__image-list > li')
-    const liIndicatorArray = carousel.querySelectorAll('.carousel__indicator > li')
-    const subtitle = carousel.querySelector('.carousel__subtitle')
+  const { carousel } = job
+  const liArray = carousel.querySelectorAll('.carousel__image-list > li')
+  const liIndicatorArray = carousel.querySelectorAll('.carousel__indicator > li')
+  const subtitle = carousel.querySelector('.carousel__subtitle')
 
-    liArray.forEach((li, i) => {
-        if (i === index) {
-            li.classList.add('active')
-        } else {
-            li.classList.remove('active')
-        }
-    })
+  liArray.forEach((li, i) => {
+    if (i === index) {
+      li.classList.add('active')
+    } else {
+      li.classList.remove('active')
+    }
+  })
 
-    liIndicatorArray.forEach((li, i) => {
-        if (i === index) {
-            li.classList.add('active')
-        } else {
-            li.classList.remove('active')
-        }
-    })
+  liIndicatorArray.forEach((li, i) => {
+    if (i === index) {
+      li.classList.add('active')
+    } else {
+      li.classList.remove('active')
+    }
+  })
 
-    subtitle.innerText = job.image.subtitles[index]
+  subtitle.innerText = job.image.subtitles[index]
 }
