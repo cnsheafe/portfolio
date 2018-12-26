6a75cf35efd26ad79837ede4acedc66a5fd1e5a9
Chris <cnsheafe@gmail.com>
2018-12-25 22:14:07 -0700

    check that sass and serve are installed globally

--- dev.sh
+++ dev.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Make sure to have "sass" and "serve" installed globally
-# npm install -g sass serve
+npm ls -g sass serve || npm install -g sass serve
 
 # Execute "sass" and then fork a new process to start "serve"
 sass sass/main.scss docs/style.css --watch --no-source-map & serve docs
