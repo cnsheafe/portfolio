087563e151d94a10e214863c62e28fff50edd38c
Chris <cnsheafe@pm.me>
2019-10-31 21:43:35 -0600

    Add links and begin styling homepage
    todos:
    - add <header> that is fixed to the top
    - color text
    - add line-spacing

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (init, main, update, view)
 
 import Browser
 import Html exposing (Html, br, button, div, h1, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
 
@@ -35,12 +36,22 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    Html.h1 []
-        [ text "Hi, I'm Chris!\n"
-        , br [] []
-        , text "Frontend Web Developer."
-        , br [] []
-        , text "React & Typescript Nerd."
-        , br [] []
-        , text "Current Obsession: Clojure & Elm."
+    Html.main_ []
+        [ Html.nav []
+            [ Html.a [] [ text "Home" ]
+            , Html.a [] [ text "About" ]
+            , Html.a [] [ text "Work Experience" ]
+            , Html.a [] [ text "Side Projects" ]
+            , Html.a [] [ text "Github" ]
+            , Html.a [] [ text "Contact" ]
+            ]
+        , Html.h1 [ class "greeting" ]
+            [ text "Hi, I'm Chris!\n"
+            , br [] []
+            , text "Frontend Web Developer."
+            , br [] []
+            , text "React & Typescript Nerd."
+            , br [] []
+            , text "Current Obsession: Clojure & Elm."
+            ]
         ]
